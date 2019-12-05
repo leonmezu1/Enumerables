0b12c431e6902b6e3d0f8489fc790719e729b965
leomezu1 <leo.mez@outlook.com>
2019-12-05 15:14:16 -0500

    Set project basis for all the methods

---
+++ enumerable.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# doc comment
+module Enumerable
+  def my_each
+    nil
+  end
+
+  def my_each_with_index
+    nil
+  end
+
+  def my_select
+    nil
+  end
+
+  def my_all?
+    nil
+  end
+
+  def my_any?
+    nil
+  end
+
+  def my_none?
+    nil
+  end
+
+  def my_count
+    nil
+  end
+
+  def my_map
+    nil
+  end
+
+  def my_inject
+    nil
+  end
+end
