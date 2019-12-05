057ee01eac5b0e3d2c94cd880f3ff24fcb1f753c
leomezu1 <leo.mez@outlook.com>
2019-12-05 17:54:23 -0500

    my_all? method done

--- enumerable.rb
+++ enumerable.rb
@@ -1,64 +1,69 @@
-include Enumerable
-
 # frozen_string_literal: true
 
 # doc comment
-module Enumerable
-  def my_each(input, index = 0)
-    output = []
-    range = input.length - index
-    range.times do
-      output.push(yield(input[index]))
-      index += 1
-    end
-    output
-  end
 
-  def my_each_with_index(input, index = 0)
-    output = []
-    range = input.length - index
-    range.times do
-      output.push("#{yield(input[index])}. #{index}")
-      index += 1
-    end
-    output
+def my_each(input, index = 0)
+  output = []
+  range = input.length - index
+  range.times do
+    output.push(yield(input[index]))
+    index += 1
   end
+  output
+end
 
-  def my_select(input, index = 0)
-    output = []
-    range = input.length - index
-    range.times do
-      output.push(input[index]) if yield(input[index])
-      index += 1
-    end
-    output
+def my_each_with_index(input, index = 0)
+  output = []
+  range = input.length - index
+  range.times do
+    output.push("#{yield(input[index])}. #{index}")
+    index += 1
   end
+  output
+end
 
-  def my_all?
-    nil
+def my_select(input, index = 0)
+  output = []
+  range = input.length - index
+  range.times do
+    output.push(input[index]) if yield(input[index])
+    index += 1
   end
+  output
+end
 
-  def my_any?
-    nil
+def my_all?(input, index = 0)
+  trigger = true
+  output = true
+  range = input.length
+  while trigger && index < range
+    trigger && output = false if yield(input[index]) == false
+    index += 1
   end
+  output
+end
 
-  def my_none?
-    nil
-  end
+def my_any?
+  nil
+end
 
-  def my_count
-    nil
-  end
+def my_none?
+  nil
+end
 
-  def my_map
-    nil
-  end
+def my_count
+  nil
+end
 
-  def my_inject
-    nil
-  end
+def my_map
+  nil
+end
+
+def my_inject
+  nil
 end
 
 # puts my_each([1, 2, 3, 4, 5, 6], 3) { |x| x * 3 }
 # puts my_each_with_index([1, 2, 3, 4, 5, 6]) { |x| x * 3 }
 # puts my_select([1, 2, 3, 4, 5, 6]) { |x| x >= 1 }
+puts my_all?([1, 2, 3, 4, 5, 6]) { |x| x >= 2 }
