64a5e1a3cd9cd3ce3b356b646e933e9a1405ab86
Leonardo Mezu <leo.mez@outlook.com>
2020-01-09 11:55:58 -0500

    Method inspections skeleton done

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require './enumerable.rb'
+
+RSpec.describe 'Enumerables' do
+  let(:arr0) { [0, 0, 1] }
+  let(:arr1) { [1, 2, 3, 4, 5] }
+  let(:arr2) { [true, 'all', string, 0.2, 0.578, 'max'] }
+  let(:arr3) { %w[lword again a word] }
+  let(:arr4) { [false, false] }
+  let(:arr5) { (1..10) }
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+
+  describe '#my_each' do
+    it '' do
+			# statement
+		end
+  end
+end
